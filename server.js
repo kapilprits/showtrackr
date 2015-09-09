7f0e23b1e952c0e9818a7ca84734c73d4e7b1c48
Prashanth Chandra <coolshanth94@gmail.com>
2015-09-09 18:49:57 +0800

    Add subscribe and unsubscribe routes

--- server.js
+++ server.js
@@ -232,6 +232,29 @@ app.post('/api/login', passport.authenticate('local'), function(req, res) {
   res.send(req.user);
 });
 
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    show.subscribers.push(req.user.id);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    var index = show.subscribers.indexOf(req.user.id);
+    show.subscribers.splice(index, 1);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
 app.get('/api/logout', function(req, res, next) {
   req.logout();
   res.send(200);
